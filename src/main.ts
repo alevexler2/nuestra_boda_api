c1b6c384b8a63237af86dd0f828e05e644bc445e
¨alevexler2@gmail.com¨ <alevexler2@gmail.com>
2025-11-04 19:51:34 -0300

    add readme

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
